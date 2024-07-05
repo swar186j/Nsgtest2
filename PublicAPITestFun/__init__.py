7603bc81db3f983d8dce216a5df766a8cbe445d9
swar186j <74865434+swar186j@users.noreply.github.com>
2024-07-05 15:57:57 +0530

    Update and rename function_app.py to PublicAPITestFun/__init__.py

--- PublicAPITestFun/__init__.py
+++ PublicAPITestFun/__init__.py
@@ -5,7 +5,7 @@ import requests
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger_poc")
-def http_trigger_poc(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     # You can customize the payload here
     payload = {
