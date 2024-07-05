6672c3d98eca64688f217618001dd7cc2004c4dc
swar186j <74865434+swar186j@users.noreply.github.com>
2024-07-05 15:40:48 +0530

    Update __init__.py

--- PublicAPITestFun/__init__.py
+++ PublicAPITestFun/__init__.py
@@ -1,3 +1,27 @@
+import azure.functions as func
+import logging
+import requests
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="http_trigger_poc")
+def http_trigger_poc(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    # You can customize the payload here
+    payload = {
+        "name": "Sample Object",
+        "data": "This is some sample data."
+    }
+    response = requests.post("https://api.restful-api.dev/objects", json=payload)
+    if response.status_code == 201 or response.status_code == 201:  # Assuming 201 Created is the success status code
+        logging.info('Successfully executed the function',response.text)
+        return func.HttpResponse(f"Success: {response.text}", status_code=201)
+    else:
+        logging.info('Failed to execute')
+        return func.HttpResponse(f"Failed: {response.text}", status_code=response.status_code)
+
+
+
 # import logging
 # import azure.functions as func
 # import http.client
